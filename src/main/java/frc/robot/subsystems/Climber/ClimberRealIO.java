d4c7c02536d641b18ea210c22995de19b28db980
Chris B <115883974+ChrisB5561@users.noreply.github.com>
2025-02-08 15:06:55 -0500

    more climber and auton

--- src/main/java/frc/robot/subsystems/Climber/ClimberRealIO.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberRealIO.java
@@ -2,39 +2,100 @@ package frc.robot.subsystems.Climber;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 // import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.miscConstants;
 
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 
 
-public class ClimberRealIO {//implements ClimberBaseIO{
+public class ClimberRealIO{ //implements ClimberBaseIO{
 
-//     private SparkMax ClimberMotor;
-//     private ClosedLoopConfig PIDConfig;
+  private SparkMax ClimberMotor;
+  private SparkMaxConfig ClimberConfig;
+  private SparkClosedLoopController closedLoopController;
+  private RelativeEncoder relativeEncoder;
 
-// public ClimberRealIO(){
+  
+public ClimberRealIO(){
 
-//     ClimberMotor = new SparkMax(30, MotorType.kBrushless);
+    ClimberMotor = new SparkMax(14, MotorType.kBrushless);
+    relativeEncoder = ClimberMotor.getEncoder();
 
-//     SparkMaxConfig globalConfig = new  SparkMaxConfig();
-//     SparkMaxConfig climberMotorConfig = new SparkMaxConfig();
-//     SparkMaxConfig pIDConfig = new SparkMaxConfig();
+    SparkMaxConfig globalConfig = new  SparkMaxConfig();
+    SparkMaxConfig ClimberConfig = new SparkMaxConfig();
+    closedLoopController = ClimberMotor.getClosedLoopController();
 
-//     globalConfig
-//     .smartCurrentLimit(30)
-//     .idleMode(IdleMode.kBrake);
+    ClimberConfig.encoder
+        .positionConversionFactor(1)
+        .velocityConversionFactor(1);
 
+    globalConfig
+    .smartCurrentLimit(30)
+    .idleMode(IdleMode.kBrake);
 
+     ClimberConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // Set PID values for position control. We don't need to pass a closed loop
+        // slot, as it will default to slot 0.
+        .p(0.1)
+        .i(0)
+        .d(0)
+        .outputRange(-1, 1)
+        // Set PID values for velocity control in slot 1
+        .p(0.0001, ClosedLoopSlot.kSlot1)
+        .i(0, ClosedLoopSlot.kSlot1)
+        .d(0, ClosedLoopSlot.kSlot1)
+        .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
+        .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
 
+    ClimberMotor.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
+    SmartDashboard.setDefaultNumber("Target Position", 0);
+    SmartDashboard.setDefaultNumber("Target Velocity", 0);
+    SmartDashboard.setDefaultBoolean("Control Mode", false);
+    SmartDashboard.setDefaultBoolean("Reset Encoder", false);
 
-// }
+}
+
+public void RunRPM(){
+
+     if (SmartDashboard.getBoolean("Control Mode", false)) {
+      /*
+       * Get the target velocity from SmartDashboard and set it as the setpoint
+       * for the closed loop controller.
+       */
+      double targetVelocity = 500; //SmartDashboard.getNumber("Target Velocity", 0);
+      closedLoopController.setReference(targetVelocity, ControlType.kVelocity, ClosedLoopSlot.kSlot1);
+    } else {
+      /*
+       * Get the target position from SmartDashboard and set it as the setpoint
+       * for the closed loop controller.
+       */
+      double targetPosition = SmartDashboard.getNumber("Target Position", 0);
+      closedLoopController.setReference(targetPosition, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    }
+
+     // Display encoder position and velocity
+     SmartDashboard.putNumber("Actual Position", relativeEncoder.getPosition());
+     SmartDashboard.putNumber("Actual Velocity", relativeEncoder.getVelocity());
+ 
+     if (SmartDashboard.getBoolean("Reset Encoder", false)) {
+       SmartDashboard.putBoolean("Reset Encoder", false);
+       // Reset the encoder position to 0
+       relativeEncoder.setPosition(0);
+     }
+}
 }
--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -104,7 +104,7 @@ public class SwerveSubsystem extends SubsystemBase
     swerveDrive.setCosineCompensator(false);//!SwerveDriveTelemetry.isSimulation); // Disables cosine compensation for simulations since it causes discrepancies not seen in real life.
     swerveDrive.setAngularVelocityCompensation(true,
                                                true,
-                                               0.1); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
+                                               0.5); //Correct for skew that gets worse as angular velocity increases. Start with a coefficient of 0.1.
     swerveDrive.setModuleEncoderAutoSynchronize(false,
                                                 1); // Enable if you want to resynchronize your absolute encoders and motor encoders periodically when they are not moving.
 //    swerveDrive.pushOffsetsToEncoders(); // Set the absolute encoder to be used over the internal encoder and push the offsets onto it. Throws warning if not possible
@@ -273,14 +273,14 @@ public class SwerveSubsystem extends SubsystemBase
   {
 // Create the constraints to use while pathfinding
     PathConstraints constraints = new PathConstraints(
-        swerveDrive.getMaximumChassisVelocity(), 4.0,
+        swerveDrive.getMaximumChassisVelocity(), 1.0,
         swerveDrive.getMaximumChassisAngularVelocity(), Units.degreesToRadians(720));
 
 // Since AutoBuilder is configured, we can use it to build pathfinding commands
     return AutoBuilder.pathfindToPose(
         pose,
         constraints,
-        edu.wpi.first.units.Units.MetersPerSecond.of(0) // Goal end velocity in meters/sec
+        edu.wpi.first.units.Units.MetersPerSecond.of(1.0) // Goal end velocity in meters/sec
                                      );
   }
 
@@ -606,7 +606,7 @@ public class SwerveSubsystem extends SubsystemBase
    */
   public void setMotorBrake(boolean brake)
   {
-    brake = false;
+    brake = true;
     swerveDrive.setMotorIdleMode(brake);
   }
 
