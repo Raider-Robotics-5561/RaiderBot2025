827cec0458233f5b0a921b500c1527ae7b7c287d
davidis2fast4u <164081810+davidis2fast4u@users.noreply.github.com>
2025-02-15 13:43:32 -0500

    added leds

--- src/main/java/frc/robot/subsystems/LEDS/LedSUB.java
+++ src/main/java/frc/robot/subsystems/LEDS/LedSUB.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems.LEDS;
 
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+
 public class LedSUB {
+  AddressableLED led;
+  AddressableLEDBuffer ledBuffer;
+  
+ 
+  public LedSUB(){
+    led = new AddressableLED (0);
+    ledBuffer = new AddressableLEDBuffer(20);
+    led.setLength(20);
+    led.setLength(ledBuffer.getLength());
+    led.start();
     
-}
+  }
+
+  }
+  
+ 
+ 
