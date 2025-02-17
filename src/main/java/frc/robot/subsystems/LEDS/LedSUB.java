df709c5bc0045db5a51022ebd7d79fb57046debf
davidis2fast4u <164081810+davidis2fast4u@users.noreply.github.com>
2025-02-17 14:13:28 -0500

    changed led subsystem

--- src/main/java/frc/robot/subsystems/LEDS/LedSUB.java
+++ src/main/java/frc/robot/subsystems/LEDS/LedSUB.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems.LEDS;
 
 
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.units.Units;
 
 
 public class LedSUB {
@@ -12,11 +15,21 @@ public class LedSUB {
  
   public LedSUB(){
     led = new AddressableLED (0);
-    ledBuffer = new AddressableLEDBuffer(20);
+    ledBuffer = new AddressableLEDBuffer(500);
     led.setLength(500);
     led.setLength(ledBuffer.getLength());
     led.start();
     
+  }
+  private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
+
+  private static final Distance kLedSpacing = Units.Meters.of(1 / 120.0);
+
+  private final LEDPattern m_scrollingRainbow = m_rainbow.scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1), kLedSpacing);
+
+  public void ledPeriodic() {
+    m_scrollingRainbow.applyTo(ledBuffer);
+    led.setData(ledBuffer);
   }
 
   }
