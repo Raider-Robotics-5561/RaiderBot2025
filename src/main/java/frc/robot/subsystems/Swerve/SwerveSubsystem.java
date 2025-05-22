c7b1cfdd2f0cd3aab85757db2f93f7de441c810c
Chris <115883974+Darkwolf7898@users.noreply.github.com>
2025-05-21 20:59:44 -0400

    set vision to True (forgot) + updated YAGSL

--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -59,9 +59,9 @@ public class SwerveSubsystem extends SubsystemBase
   /**
    * Enable vision odometry updates while driving.
    */
-  private final boolean     visionDriveTest = false;
+  private final boolean     visionDriveTest = true;
   /**
-   * PhotonVision class to keep an accurate odometry.
+   * PhotonVision class to keep an accurate odometry .
    */
   private       Vision      vision;
 
